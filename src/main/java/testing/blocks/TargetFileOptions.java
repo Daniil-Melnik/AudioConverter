945bd46e6cdfadf9e976756716e4960bc28a7fb1
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-05-26 15:02:59 +0300

    added lombok

--- src/main/java/testing/blocks/TargetFileOptions.java
+++ src/main/java/testing/blocks/TargetFileOptions.java
@@ -1,11 +1,15 @@
 package testing.blocks;
 
+import lombok.Getter;
+import lombok.Setter;
 import testing.dataclasses.ChannelInfo;
 import testing.dataclasses.enums.FileExtensions;
 import testing.dataclasses.strategies.channels.AvaliableChannels;
 import testing.dataclasses.strategies.codecs.AvaliableCodecs;
 import testing.dataclasses.strategies.rates.AvaliableRates;
 
+@Getter
+@Setter
 public class TargetFileOptions {
     private AvaliableChannels avaliableChannelsStrategy; // стратегии для определения доступныйх вариантов расширений
     private AvaliableCodecs avaliableCodecsStrategy;
@@ -14,5 +18,8 @@ public class TargetFileOptions {
     private FileExtensions targetExtension;
     private String targetCodec;
     private ChannelInfo targetChannelInfo;
+    private String targetBitRate;
+    private String targetSampleSize;
+    private String targetDirectory;
     // доработать
 }
